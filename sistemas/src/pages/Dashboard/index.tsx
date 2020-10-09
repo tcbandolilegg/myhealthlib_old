f0d3888404e70c5cfc49abe82fbfeac1964a8f0e
Thereza Bandoli <tcbandolilegg@gmail.com>
2020-10-09 20:20:17 -0300

    trabalhando no dashboard

--- sistemas/src/pages/Dashboard/index.tsx
+++ sistemas/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Header from '../../components/Header';
+
 import api from '../../services/api';
 
 import { useAuth } from '../../hooks/auth';
@@ -18,7 +18,6 @@ interface Usuarios {
   enderecoBairro: string;
   codEstado: number;
   codCidade: number;
-
   userDependente1: number;
   userDependente2: number;
   userDependente3: number;
@@ -53,53 +52,46 @@ const Dashboard: React.FC = () => {
         <Title>Dados Cadastrados</Title>
 
         <TableContainer>
-          <table width="100%">
-            {/* <table width="100%"> */}
+          <table>
             <thead>
               <tr>
                 <th>ID</th>
               </tr>
             </thead>
+            <tr>
+              <th>CPF:</th>
+            </tr>
+            <tr>
+              <th>Nome:</th>
+            </tr>
+            <tr>
+              <th>Data nascimento:</th>
+            </tr>
+            <tr>
+              <th>Endereço:</th>
+            </tr>
+            <tr>
+              <th>Bairro:</th>
+            </tr>
+            <tr>
+              <th>Estado:</th>
+            </tr>
+            <tr>
+              <th>Dependente 1:</th>
+            </tr>
+            <tr>
+              <th>Dependente 2:</th>
+            </tr>
+            <tr>
+              <th>Dependente 3:</th>
+            </tr>
+            <tr>
+              <th>Dependente 4:</th>
+            </tr>
+            <tr>
+              <th>Dependente 5:</th>
+            </tr>
             <tbody>
-              <tr>
-                <th>CPF:</th>
-              </tr>
-              <tr>
-                <th>Nome:</th>
-              </tr>
-              <tr>
-                <th>Data nascimento:</th>
-              </tr>
-              <tr>
-                <th>Endereço:</th>
-              </tr>
-              <tr>
-                <th>Bairro:</th>
-              </tr>
-              <tr>
-                <th>Estado:</th>
-              </tr>
-              <tr>
-                <th>Dependente 1:</th>
-              </tr>
-              <tr>
-                <th>Dependente 2:</th>
-              </tr>
-              <tr>
-                <th>Dependente 3:</th>
-              </tr>
-              <tr>
-                <th>Dependente 4:</th>
-              </tr>
-              <tr>
-                <th>Dependente 5:</th>
-              </tr>
-            </tbody>
-            {/* </table>
-
-          <table> */}
-            <tbody>
-              {/* // me peerdi aqi qual é com letra maiuscula */}
               {usuarios.map(usuarios => (
                 <tr key={usuarios.id}>
                   <td>{usuarios.id}</td>
@@ -107,8 +99,8 @@ const Dashboard: React.FC = () => {
                   <td>{usuarios.nome}</td>
                   <td>{usuarios.dataNascimento}</td>
                   <td>
-                    {usuarios.tipoLogradouro},{usuarios.enderecoLogradouro},
-                    {usuarios.enderecoNumero},{usuarios.enderecoBairro}
+                    {usuarios.tipoLogradouro} {usuarios.enderecoLogradouro}
+                    {usuarios.enderecoNumero} {usuarios.enderecoBairro}
                   </td>
                   <td>{usuarios.codEstado}</td>
                   <td>{usuarios.codCidade}</td>
